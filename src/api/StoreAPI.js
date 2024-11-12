ead3394a63ffc59760e301697da3d3b7a710ca9d
MAslan <m.aslan027@hotmail.com>
2024-11-12 11:18:48 +0100

    yorum satirlari eklendi

--- src/api/StoreAPI.js
+++ src/api/StoreAPI.js
@@ -5,7 +5,7 @@ const baseUrl = "https://www.adidas.de/api/stores";
 export const fetchStores = async (latitude, longitude) => {
   try {
     const response = await axios.get(
-      `${baseUrl}?latitude=${latitude}&longitude=${longitude}`
+      `${baseUrl}?latitude=${latitude}&longitude=${longitude}` // template literal 
     );
     return response.data.content;
   } catch (error) {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")); // id si root olan elementi sec
 root.render(
   <React.StrictMode>
     <App />
